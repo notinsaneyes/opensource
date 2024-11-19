0fccfe9cdd5cc3fe4d9ff04f73680a0e3ebec987
notinsaneyes <141480410+notinsaneyes@users.noreply.github.com>
2024-11-19 12:08:25 +0530

    Create rps.py

---
+++ visa-prep/rps.py
@@ -0,0 +1,9 @@
+def f(v,c):
+    if (v=='R' and c=='P') or (v=='P' and c=='S') or (v=='S' and c=='R'):
+        print("Charan")
+    elif(v=='P' and c=='R') or (v=='S' and c=='P') or (v=='R' and c=='S'):
+        print("Vignesh")
+    else:
+        print("NULL")
+v,c=map(str,input().split())
+f(v,c)
